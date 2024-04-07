fcbd3cf145bf701785f522f967f4a1c26fbd73d2
unknown <maxezay94@gmail.com>
2024-04-07 01:02:30 -0300

    Leyendo los datos de los cursos seleccionados

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// Variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtm = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners() {
+    // Cuando se agrega un curso al presionar "Agregar al Carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+// Funciones
+function agregarCurso(e) {
+    e.preventDefault()
+
+    if (e.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+
+}
+
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso);
+}
